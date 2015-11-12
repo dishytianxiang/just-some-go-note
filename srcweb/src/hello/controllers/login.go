49b462520a6f199e532a6cd1b3be837db79b2c29
dishytianxiang <1066844321@qq.com>
2015-11-12 17:40:28 +0800

    hello

---
+++ srcweb/src/hello/controllers/login.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type LoginController struct {
+	beego.Controller
+}
+
+func (this *LoginController) Get() {
+	this.TplNames = "login.html"
+}
+func (this *LoginController) Post() {
+
+}
