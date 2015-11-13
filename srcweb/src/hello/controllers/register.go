eee899616fa863317bcbd999289a23a944acb7d3
dishytianxiang <1066844321@qq.com>
2015-11-13 16:09:03 +0800

    two

---
+++ srcweb/src/hello/controllers/register.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type RegisterController struct {
+	beego.Controller
+}
+
+func (this *RegisterController) Get() {
+	this.TplNames = "register.html"
+}
+func (this *RegisterController) Post() {
+
+}
--- srcweb/src/hello/routers/router.go
+++ srcweb/src/hello/routers/router.go
@@ -7,5 +7,5 @@ import (
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
-	beego.Router("/login", &controllers.LoginController{})
+	beego.Router("/register", &controllers.RegisterController{})
 }
